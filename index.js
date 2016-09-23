1684b21a8831c490a5b282ed5efda7b0ff0135e2
koundinyag25 <koundinyag25@gmail.com>
2016-09-23 17:19:45 -0500

    commits

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var routes = require('./server/routes');
+var expressBeautify = require('express-beautify');
+var bodyParser = require('body-parser')
+
+
+
+app.use(express.static(path.join(__dirname ,'public')));
+app.use('/',routes);
+app.use(bodyParser.json());
+
+
+app.listen('3000',function(){
+    console.log('listening on port 3000');
+});
---
+++ public/app.js
@@ -0,0 +1,32 @@
+var myApp = angular.module('myApp',['ui.router']);
+
+
+
+myApp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+        .state('home',{
+          url:'/',
+          templateUrl: 'views/home.html',
+          controller: 'HomeController'
+        });
+}]);
+
+
+myApp.controller("HomeController", ['$scope','$http','$rootScope',function($scope,$http,$rootScope){
+      $rootScope.business = [];
+
+      $scope.search = function(q){
+        var keyword = $scope.search.keyword;
+        var location = $scope.search.location;
+        $http.get('/business', { params :{ term: keyword ,location: location }}).then(function(data){
+          console.log(data.data.businesses);
+          $rootScope.business = data.data.businesses;
+          console.log($rootScope.business);
+        }).catch(function(error){
+          return error;
+        });
+      }
+
+}]);
