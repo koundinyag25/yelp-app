4e93158ef036ccd32c2caf26bbc321c8c4cc623c
koundinyag25 <koundinyag25@gmail.com>
2016-09-28 03:11:59 -0500

    v0.0.2

--- public/app.js
+++ public/app.js
@@ -1,18 +1,51 @@
-angular.module('myApp',['ui.router','myApp.services','ngMaterial','leaflet-directive'])
-.config(function($stateProvider,$urlRouterProvider,$locationProvider){
+angular.module('myApp', ['ui.router', 'myApp.services', 'ngMaterial', 'nemLogging', 'ui-leaflet'])
+    .config(function($stateProvider, $urlRouterProvider, $locationProvider,$mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+    .primaryPalette('deep-purple', {
+      'default': '600',
+      'hue-2': '500',
+      'hue-3': '100'
+    }).accentPalette('green', {
+      'default': 'A200'
+    });
+
+    $mdThemingProvider.theme('second')
+    .primaryPalette('teal',{
+      default: '500',
+      'hue-1': '500',
+      'hue-2': '200',
+      'hue-3': '100'
+
+    }).accentPalette('red',{
+      default: 'A200'
+    });
+
+
+    $mdThemingProvider.theme('third')
+    .primaryPalette('grey',{
+      default: '300',
+      'hue-1': '200',
+      'hue-2': '100',
+      'hue-3': '50'
+
+    }).accentPalette('red',{
+      default: 'A200'
+    });
+
+
 
         $stateProvider
-        .state('home',{
-          url:'/',
-          templateUrl: 'views/home1.html',
-          controller: 'HomeCtrl'
-        });
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            });
 
         $locationProvider.html5Mode({
-              enabled: true,
-              requireBase: false
-              });
-});
+            enabled: true,
+            requireBase: false
+        });
+    });
 angular.element(document).ready(function() {
-     angular.bootstrap(document, ['myApp']);
-   });
+    angular.bootstrap(document, ['myApp']);
+});
--- public/controllers/HomeController.js
+++ public/controllers/HomeController.js
@@ -1,18 +1,45 @@
-
-  angular.module('myApp').controller("HomeCtrl", ['$scope','$http','$rootScope','myAppServices',function($scope,$http,$rootScope,myAppServices){
+angular.module('myApp').controller("HomeCtrl", ['$scope', '$http', '$rootScope', 'myAppServices', 'leafletData', 'nemSimpleLogger',
+    function($scope, $http, $rootScope, myAppServices, leafletData, $timeout) {
         $scope.business = [];
         $scope.emptyArray = true;
-      $scope.search = function(){
-        myAppServices.search('movies','chicago').then(function(data){
-            $scope.business = data.data.businesses;
-            if($scope.business.length  > 0){
-               return $scope.emptyArray = false;
-            }
-            $scope.search.keyword ='';
-            $scope.search.location = '';
-        }).catch(function(error){
-          return error;
-        });
-      }
+        $scope.center = {
+          lat: -33.8830,
+              lng: 151.2166,
+              zoom:20
+        };
+        $scope.defaults = {
+           scrollWheelZoom: false
+        };
 
-  }]);
+
+
+            $scope.search = function() {
+                myAppServices.search($scope.search.keyword, $scope.search.location).then(function(data) {
+                    $scope.business = data.data.businesses;
+
+                    if ($scope.business.length > 0) {
+                        return $scope.emptyArray = false;
+                    }
+
+                    console.log($scope.business,data);
+                    // angular.extend($scope,{
+                    //   center :{
+                    //     lat: -33.8830,
+                    //     lng: 151.2166,
+                    //     zoom:20
+                    //   },
+                    //   defaults:{
+                    //     scrollWheelZoom: false
+                    //   }
+                    // });
+                    $scope.search.keyword = '';
+                    $scope.search.location = '';
+                }).catch(function(error) {
+                    return error;
+                });
+
+            }
+
+
+    }
+]);
